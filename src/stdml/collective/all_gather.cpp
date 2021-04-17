fa9fab0a1b9caa233e4cbf126a1c2aecc6d85b65
lg <lg4869@outlook.com>
2021-04-17 21:20:14 +0100

    fix: use size_t

--- src/stdml/collective/all_gather.cpp
+++ src/stdml/collective/all_gather.cpp
@@ -32,8 +32,8 @@ void session::all_gather(const void *input, void *output, size_t count,
     auto par = this->pool();
     fmap(
         par,
-        [&](int idx) {
-            int rank = idx / 2;
+        [&](size_t idx) {
+            const size_t rank = idx / 2;
             if (rank == this->rank_) {
                 if (idx % 2 == 0) {
                     ws[rank].forward();
@@ -52,6 +52,6 @@ void session::all_gather(const void *input, void *output, size_t count,
                 q->get(w.recv);
             }
         },
-        seq<int>(peers_.size() * 2));
+        seq<size_t>(peers_.size() * 2));
 }
 }  // namespace stdml::collective
