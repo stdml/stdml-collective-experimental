3b6e4c32028606225ceacd2043037e857a097f9d
lg <lg4869@outlook.com>
2021-05-22 15:39:51 +0100

    use .at

--- src/stdml/collective/send_recv.cpp
+++ src/stdml/collective/send_recv.cpp
@@ -6,7 +6,7 @@ namespace stdml::collective
 void session::recv(void *output, size_t count, dtype dt, size_t target,
                    std::string name)
 {
-    const auto &id = peers_[target];
+    const auto &id = peers_.at(target);
     auto q = this->slotbox_->require(id, name);
     q->get(output);
 }
@@ -14,7 +14,7 @@ void session::recv(void *output, size_t count, dtype dt, size_t target,
 void session::send(const void *input, size_t count, dtype dt, size_t target,
                    std::string name)
 {
-    const auto &id = peers_[target];
+    const auto &id = peers_.at(target);
     uint32_t flags = rchan::message_header::wait_recv_buf;
     auto client = this->client_pool_->require(rchan::conn_collective);
     client->send(id, name.c_str(), input, count * dtype_size(dt), flags);
